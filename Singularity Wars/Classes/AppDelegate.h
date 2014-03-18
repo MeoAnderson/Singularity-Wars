3a4d1eb934461225f98f0c9806b4db0cd9a51dd8
Vasco Patrício <meoanderson4@gmail.com>
2014-03-18 17:36:15 +0000

    Initial Commit

---
+++ Singularity Wars/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Singularity Wars
+//
+//  Created by Vasco Patrício on 18/03/14.
+//  Copyright Vasco Patrício 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
